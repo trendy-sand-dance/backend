925211347b18eb32d831319827c6cf43d7304abe
smclacke <sarahmclacken@gmail.com>
2025-03-21 13:39:15 +0100

    db up and running, user management stuff moved out

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -8,18 +8,6 @@ import {delDev} from "../controllers/dev/delDev.controller";
 import {changeStatusDev} from "../controllers/dev/changeStatusDev.controller";
 import {editDev} from "../controllers/dev/editDev.controller";
 
-// user
-import {registerUser} from "../controllers/user/registerUser.controller";
-import {loginUser} from "../controllers/user/loginUser.controller";
-import {logout} from "../controllers/user/logout.controller";
-import {deleteUser} from "../controllers/user/deleteUser.controller";
-import {editUser} from "../controllers/user/editUser.controller";
-
-// avatar
-import {addAvatar} from "../controllers/user/avatar/addAvatar.controller";
-import {editAvatar} from "../controllers/user/avatar/editAvatar.controller";
-import {deleteAvatar} from "../controllers/user/avatar/deleteAvatar.controller";
-
 // web
 import {getDashUser} from "../controllers/web/getDashUser.controller";
 
@@ -32,18 +20,6 @@ async function routes(fastify: FastifyInstance) {
 	fastify.get('/del', delDev);
 	fastify.get('/stat', changeStatusDev);
 	fastify.get('/eddy', editDev);
-
-	// user
-	fastify.post('/register', registerUser);
-	fastify.post('/login', loginUser);
-	fastify.post('/logout', logout);
-	fastify.post('/edit', editUser);
-	fastify.delete('/delete', deleteUser);
-
-	// avatar
-	fastify.post('/addAvatar', addAvatar);
-	fastify.post('/editAvatar', editAvatar);
-	fastify.delete('/deleteAvatar', deleteAvatar);
 
 	// web
 	fastify.get('/dash/:username', getDashUser);
